8b4b0bd403202f81eb2fdeab07f0d48d6c6a7726
Noah Hoffman <noah.hoffman@gmail.com>
2011-04-08 12:27:18 -0700

    logging verbosity is set in tests/config.py

--- taxonomy/tests/config.py
+++ taxonomy/tests/config.py
@@ -1,7 +1,23 @@
 import os
+import sys
 import logging
+import re
 
-debuglevel = logging.WARNING
+# set verbosity of logging output
+try:
+    logflag = re.findall(r'-[vq]+\b', ' '.join(sys.argv[1:]))[0]
+except IndexError:
+    logflag = ''
+
+logging.basicConfig(
+    file = sys.stdout,
+    format = '%(levelname)s %(module)s %(lineno)s %(message)s' \
+        if logflag.startswith('-v') else '%(message)s',
+    level = {'-q':logging.ERROR,
+             '':logging.WARNING,
+             '-v': logging.INFO,
+             '-vv': logging.DEBUG}[logflag]
+    )
 
 datadir = 'testfiles'
 outputdir = 'test_output'
